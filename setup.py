03dc1411339d3baafb7807b93c4ffcb4c2c45501
Dmitrii Gerasimenko <kiddima@gmail.com>
2016-08-12 23:13:02 +0300

    version up to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-happymailer',
-    version='0.1.3',
+    version='0.1.4',
     description='django email templates manager',
     author='Victor Kotcheruba',
     author_email='barbuzaster@gmail.com',
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'django >= 1.9',
         'django-import-export == 0.4.5',
-        'fake-factory == 0.5',
+        'fake-factory == 0.5.7',
         'html2text == 2016.5.29',
         'trafaret >= 0.7',
         'six',
