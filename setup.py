75607ec3beca0bb735c411bc10e01e953488f355
Victor Kotseruba <barbuzaster@gmail.com>
2016-09-15 22:08:57 +0300

    0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-happymailer',
-    version='0.1.4',
+    version='0.1.5',
     description='django email templates manager',
     author='Victor Kotcheruba',
     author_email='barbuzaster@gmail.com',
@@ -19,3 +19,4 @@ setup(
         'six',
     ]
 )
+
