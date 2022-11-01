9f2d63e7569af8ca1c831b811ec65706c9206c60
Victor Kotseruba <victor@tutorme.com>
2022-11-01 13:00:28 -0700

    version 0.4.1, django 4+ support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-happymailer',
-    version='0.4.0',
+    version='0.4.1',
     description='django email templates manager',
     author='Victor Kotcheruba',
     author_email='barbuzaster@gmail.com',
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     packages=find_packages(exclude=['django_happymailer','dummy','dummy2']),
     install_requires=[
-        'django >= 3.2.4, < 4.0',
+        'django >= 3.2.4, < 5.0',
         'django-import-export >= 0.4.5',
         'faker >= 0.8.6',
         'html2text >= 2016.5.29',
